2d033b4ae384a34a9dda95214c2e6f20cb1ab22f
vinay kumar <vinaythamtam@outlook.com>
2018-08-16 00:19:34 +0530

    updated location page & added icons

--- src/pages/location-pick/location-pick.ts
+++ src/pages/location-pick/location-pick.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,NgZone } from '@angular/core';
 import { App, IonicPage, NavController, NavParams, Platform,ModalController, ViewController  } from 'ionic-angular';
 import {
   LocationService,
@@ -7,7 +7,11 @@ import {
   GoogleMapOptions,
   MyLocation,
   Marker,
-  GoogleMapsEvent
+  GoogleMapsEvent,
+  CameraPosition,
+  LatLng,
+  Geocoder,
+  GeocoderResult
 } from '@ionic-native/google-maps';
 
 /**
@@ -24,9 +28,10 @@ import {
 })
 export class LocationPickPage {
   map: GoogleMap;
+  address:string;
   constructor(public navCtrl: NavController, public navParams: NavParams,
      private platform: Platform,public modalCtrl: ModalController,public viewCtrl: ViewController,
-     public app: App) {
+     public app: App,private _ngZone:NgZone) {
       
   }
 
@@ -60,10 +65,56 @@ export class LocationPickPage {
         title:myLocation.latLng.lat+' - '+myLocation.latLng.lng,
         position:myLocation.latLng,
         animation:'drop',
-        draggable:true 
+        visible:true
       });
           marker.showInfoWindow();
       
+          this.map.on(GoogleMapsEvent.CAMERA_MOVE_END).subscribe((params: any[]) => {
+            let cameraPosition: CameraPosition<LatLng> = params[0];
+            
+            Geocoder.geocode({
+              "position": cameraPosition.target
+            }).then((results: GeocoderResult[]) => {
+              if (results.length == 0) {
+                // Not found
+                return null;
+              }
+              // let address: any = [
+              //   results[0].subThoroughfare || "",
+              //   results[0].thoroughfare || "",
+              //   results[0].locality || "",
+              //   results[0].adminArea || "",
+              //   results[0].postalCode || "",
+              //   results[0].country || ""].join(", ");
+
+                console.log(results[0].locale);
+                console.log(results[0].adminArea);
+                console.log(results[0].country);
+                console.log(results[0].countryCode);
+                console.log(results[0].extra.featureName);
+                console.log(results[0].extra.lines);
+                console.log(results[0].extra.permises);
+                console.log(results[0].extra.phone);
+                console.log(results[0].locality);
+                console.log(results[0].postalCode);
+                console.log(results[0].subAdminArea);
+                console.log(results[0].subLocality);
+                console.log(results[0].subThoroughfare);
+                console.log(results[0].thoroughfare);
+              //marker.setPosition(cameraPosition.target);
+              //marker.setTitle(address);
+              //marker.showInfoWindow();
+              //this.address=address;
+              this._ngZone.run(() => {
+                this.address = results[0].extra.lines.join(',');
+          });
+            });
+
+
+            // marker.setPosition(cameraPosition.target);
+            // marker.setTitle(cameraPosition.target.lat+' - '+cameraPosition.target.lng);
+             console.log(cameraPosition.target.lat+' - '+cameraPosition.target.lng);
+          });
     });
   }
   onMapReady() {
